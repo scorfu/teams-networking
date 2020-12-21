64146cc84076f265c86005d62e584cabd54016a4
scorfu <you@example.com>
2020-12-21 21:41:40 +0200

    let's call it  day

--- functions1.js
+++ functions1.js
@@ -1,4 +1,3 @@
-
 function insertPersons(persons) {
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons);
@@ -17,13 +16,33 @@ function getPersonHtml (person) {
     </tr>`;
 }
 
-
+let allPersons = [];
 
 fetch('persons.json')
     .then(res => res.json())
     .then(team => {
+        allPersons = team
         insertPersons(team);
     });
+
+function searchPersons(text) {
+    text = text.toLowerCase()
+    console.warn('ce?', text);
+    return allPersons.filter(person => {
+        return person.firstName.toLowerCase().indexOf(text) > -1 || 
+    person.lastName.toLowerCase().indexOf(text) > -1;
+    })
+}
+
+const search = document.getElementById('search');
+search.addEventListener('input', e => {
+    const text = e.target.value
+    
+    const filtrate = searchPersons(text);
+
+    insertPersons(filtrate);
+})
+
 
 
 
